76139c74fdcd59c10d64ac66f076ff743d2a1f5f
Aldar Pärnoja <aldar.parnoja@tptlive.ee>
2018-11-27 16:08:39 +0200

    parandatud

--- admin/reg.php
+++ admin/reg.php
@@ -1,25 +1,32 @@
-<?php
-    $conn = mysqli_connect('localhost', 'root', '');
-    
-    if (isset($_POST['sisesta'])) {
-        $user = $_POST['kasutaja'];
-        $pass = ($_POST['parool']);
-    
-        $sql = "INSERT INTO ms17.users(username, password) VALUES ($_POST['kasutaja'],$_POST['parool'])";
-        mysqli_query($conn, $sql);
-    }
-
-?>
-
 <!DOCTYPE html>
 <body>
     <h1>Registreeri</h1>
     <form method="post">
         <input type="text" name="kasutaja" placeholder="kasutajanimi">
         <input type="password" name="parool" placeholder="parool">
-        <input type="password" name="parool" placeholder="parooli kordus">
+        <input type="password" name="parool2" placeholder="parooli kordus">
         <button type="submit" name="register">Registreeru</button>
         <button type="reset">Puhasta valjad</button>
     </form>
 </body>
 </html>
+
+
+<?php
+    $conn = mysqli_connect('localhost', 'root', '');
+    
+    if (isset($_POST['register'])) {
+        $user = $_POST['kasutaja'];
+        $pass = ($_POST['parool']);
+
+        echo var_dump($_POST);
+    
+        $sql = "INSERT INTO ms17.users
+        (username, password) VALUES 
+        ('$_POST[kasutaja]' , '$_POST[parool]')";
+
+        echo var_dump($sql);
+        mysqli_query($conn, $sql);
+    }
+
+?>
